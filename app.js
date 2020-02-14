831c2c301c225b957a0c83cc914c65b88e73b82e
Artur Gawron <artur@gawron.me>
2020-02-14 19:36:53 +0100

    Add event listeners

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
+
+//FETCH RATES and UPDATE the DOM
+function calculate() {
+
+}
+
+//EVENT LISTENERS
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_otwo.addEventListener('input', calculate);
+
+calculate();
